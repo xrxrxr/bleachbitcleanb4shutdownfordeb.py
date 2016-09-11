25d899748ba04ebf7e143ee8ede571278bc262d0
xrxrxr <xrthefrench@gmail.com>
2016-09-11 18:54:52 +0200

    Add files via upload
    the script

---
+++ bleachbitcleanb4shutdownfordeb.py
@@ -0,0 +1,306 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-       
+
+print("Hello, Priviet , Nihao , Hola")
+
+
+
+
+
+print("this script will  install & run bleachbit cleaner in terminal  and shutdown your computer, tested in DEBIAN , feel free to modify as you want ")
+
+raw_input("Press Enter to continue...")
+
+print("follow me on twitter i follow back @sincerebroth ")
+
+
+raw_input("Press Enter to install bleachbit or check that you already installed  ...")
+
+import os 
+os.system("sudo apt-get install bleachbit")
+
+
+
+
+
+
+
+
+
+raw_input("Press Enter to show all bleachbit list to add or remove editing this script...")
+
+import os 
+os.system("bleachbit --list")
+
+print("this is a list of all the bleachbit command that you can run to clean on your computer ")
+
+raw_input("Press Enter to continue...")
+
+print(" all the bleachbit command by default i chosed , again feel free to modify it , ")
+
+
+
+
+print(" /apt.autoclean/apt.autoremove/apt.clean/bash.history/chromium.cache/chromium.cookies/chromium.current_session/chromium.dom/chromium.form_history/chromium.history/chromium.search_engines/chromium.vacuum/deepscan.backup/deepscan.ds_store/deepscan.thumbs_db/deepscan.tmp/firefox.cache/firefox.cookies/firefox.crash_reports/firefox.dom/firefox.download_history/firefox.forms/firefox.passwords/firefox.session_restore/firefox.site_preferences/firefox.url_history/firefox.vacuum/clean flash.cache/flash.cookies/gimp.tmp/gnome.run/gnome.search_history/google_chrome.cache/google_chrome.cookies/google_chrome.dom/google_chrome.form_history/google_chrome.history/google_chrome.search_engines/google_chrome.session/google_chrome.vacuum/google_earth.temporary_files/java.cache/kde.cache/kde.tmp/opera.cache/‭opera.cookies/opera.current_session/opera.dom/opera.download_history/opera.search_history/opera.url_history/seamonkey.cache/seamonkey.chat_logs/‪seamonkey.cookies/‪seamonkey.download_history/seamonkey.history/skype.chat_logs/system.cache/system.clipboard/system.localizations/system.memory/system.recent_documents/system.rotated_logs/system.tmp/system.trash/thumbnails.cache/transmission.cache")
+
+
+
+
+#cutomise from here 
+
+
+
+raw_input("Press Enter to start cleaning...")
+
+
+
+os.system("bleachbit --clean apt.autoclean")
+
+import os
+os.system("bleachbit --clean apt.autoremove")
+
+import os
+os.system("bleachbit --clean apt.clean")
+
+
+os.system("bleachbit --clean bash.history")
+
+
+os.system("bleachbit --clean chromium.cache")
+
+
+os.system("bleachbit --clean chromium.cookies")
+
+
+os.system("bleachbit --clean chromium.current_session")
+
+
+os.system("bleachbit --clean chromium.dom")
+
+
+os.system("bleachbit --clean chromium.form_history")
+
+
+os.system("bleachbit --clean chromium.history")
+
+
+os.system("bleachbit --clean chromium.search_engines")
+
+
+os.system("bleachbit --clean chromium.vacuum")
+
+
+os.system("bleachbit --clean deepscan.backup")
+
+
+os.system("bleachbit --clean deepscan.ds_store")
+
+
+os.system("bleachbit --clean deepscan.thumbs_db")
+
+
+os.system("bleachbit --clean deepscan.tmp")
+
+
+os.system("bleachbit --clean firefox.cache")
+
+
+os.system("bleachbit --clean firefox.cookies")
+
+
+os.system("bleachbit --clean firefox.crash_reports")
+
+
+os.system("bleachbit --clean firefox.dom")
+
+
+os.system("bleachbit --clean firefox.download_history")
+
+
+
+os.system("bleachbit --clean firefox.forms")
+
+
+os.system("bleachbit --clean firefox.passwords")
+
+
+
+os.system("bleachbit --clean firefox.session_restore")
+
+
+os.system("bleachbit --clean firefox.site_preferences")
+
+
+os.system("bleachbit --clean firefox.url_history")
+
+
+os.system("bleachbit --clean firefox.vacuum")
+
+
+os.system("bleachbit --clean flash.cache")
+
+
+os.system("bleachbit --clean flash.cookies")
+
+
+
+os.system("bleachbit --clean gimp.tmp")
+
+
+os.system("bleachbit --clean gnome.run")
+
+
+
+os.system("bleachbit --clean gnome.search_history")
+
+
+os.system("bleachbit --clean google_chrome.cache")
+
+
+os.system("bleachbit --clean google_chrome.cookies")
+
+
+os.system("bleachbit --clean google_chrome.dom")
+
+
+os.system("bleachbit --clean google_chrome.form_history")
+
+
+os.system("bleachbit --clean google_chrome.history")
+
+
+os.system("bleachbit --clean google_chrome.search_engines")
+
+
+os.system("bleachbit --clean google_chrome.session")
+
+
+os.system("bleachbit --clean google_chrome.vacuum")
+
+
+os.system("bleachbit --clean google_earth.temporary_files")
+
+
+os.system("bleachbit --clean java.cache")
+
+
+os.system("bleachbit --clean kde.cache")
+
+
+os.system("bleachbit --clean kde.tmp")
+
+
+os.system("bleachbit --clean konqueror.cookies")
+
+
+os.system("bleachbit --clean konqueror.url_history")
+
+
+os.system("bleachbit --clean ‭opera.cache")
+
+
+os.system("bleachbit --clean ‭opera.cookies")
+
+
+os.system("bleachbit --clean opera.current_session")
+
+
+os.system("bleachbit --clean opera.dom")
+
+
+os.system("bleachbit --clean opera.download_history")
+
+
+os.system("bleachbit --clean opera.search_history")
+
+
+os.system("bleachbit --clean opera.url_history")
+
+
+os.system("bleachbit --clean seamonkey.cache")
+
+
+os.system("bleachbit --clean seamonkey.chat_logs")
+
+
+os.system("bleachbit --clean ‪seamonkey.cookies")
+
+
+os.system("bleachbit --clean ‪seamonkey.download_history")
+
+
+os.system("bleachbit --clean seamonkey.history")
+
+
+
+os.system("bleachbit --clean skype.chat_logs")
+
+
+os.system("bleachbit --clean system.cache")
+
+
+os.system("bleachbit --clean system.clipboard")
+
+
+os.system("bleachbit --clean system.localizations")
+
+
+os.system("bleachbit --clean system.memory")
+
+
+os.system("bleachbit --clean system.recent_documents")
+
+
+
+os.system("bleachbit --clean system.rotated_logs")
+
+
+os.system("bleachbit --clean system.tmp")
+
+
+os.system("bleachbit --clean system.trash")
+
+
+os.system("bleachbit --clean thumbnails.cache")
+
+
+os.system("bleachbit --clean transmission.cache")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("shutdown the computer")
+
+
+
+
+
+
+
+print("shutdown system in 10 second")
+print("shutdown system in  9 second")
+print("shutdown system in  8 second")
+print("shutdown system in  7 second")
+print("shutdown system in  6 second")
+print("shutdown system in  5 second")
+print("shutdown system in  4 second")
+print("shutdown system in  3 second")
+print("shutdown system in  2 second")
+print("shutdown system in  1 second")
+
+import os
+os.system("shutdown -h now")
